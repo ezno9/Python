a785d45429422252c314396423e402e031e835d1
ljh <ezno_9@naver.com>
2022-01-25 22:00:40 +0900

    소수 만들기

--- Level_1/PrimeNumber.py
+++ Level_1/PrimeNumber.py
@@ -1,10 +1,23 @@
 # 소수 만들기
+from itertools import combinations
+
+def prime(num):
+    if num == 0 or num == 1:
+        return False
+    else:
+        for i in range(2, (num // 2) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+
 def solution(nums):
-    answer = -1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    answer = 0
+    cmb = list(combinations(nums, 3))
+    for arr in cmb:
+        if prime(sum(arr)):
+            answer += 1
     return answer
 
-
+print(solution([1,2,3,4]))
+print(solution([1,2,7,6,4]))
